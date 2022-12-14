6240b3cfacb68c51ea0d148e6c3e78fa0e7c91cd
darksectordds <pechenskih.stas@ya.ru>
2022-12-15 00:03:41 +0300

    add app, view, list of pizza impls

---
+++ resources/js/components/modules/AppComponent.vue
@@ -0,0 +1,28 @@
+<template>
+    <view-component :navigation="navigation"></view-component>
+</template>
+
+<script>
+    import ViewComponent from "./ViewComponent";
+
+    export default {
+        name: 'AppComponent',
+        components: { ViewComponent },
+        data() {
+            return {
+                // описание ссылок под single-page app
+                // следующего формата:
+                // -------------------------------------------
+                // - icon(css),
+                // - title(text),
+                // - route(name из router.js),
+                // - params(параметры передающие в vue-компонент),
+                // - query(url-параметры),
+                // - children(список детей, как было показано чуть выше с route)
+                navigation: [
+                    { icon: null, title: 'Главная', route: 'index', params:{}, query: {}, children: [] },
+                ],
+            }
+        },
+    }
+</script>
---
+++ resources/js/components/modules/pizza/PizzaCardComponent.vue
@@ -0,0 +1,33 @@
+<template>
+    <template-pizza-card-component v-if="hasPizza"
+                                   :title="title"
+                                   :image="image"
+                                   :description="description"
+                                   :price="price"></template-pizza-card-component>
+</template>
+
+<script>
+    export default {
+        name: 'PizzaCardComponent',
+        props: {
+            pizza: {type: Object, default: () => ({})},
+        },
+        computed: {
+            hasPizza() {
+                return !!this.pizza;
+            },
+            title() {
+                return this.$models.$product.title(this.pizza);
+            },
+            image() {
+                return this.$models.$product.firstImage(this.pizza);
+            },
+            description() {
+                return this.$models.$product.description(this.pizza);
+            },
+            price() {
+                return this.$models.$product.price(this.pizza);
+            },
+        },
+    }
+</script>
