6c1b1b16f1088b3681e76ddcd52375329b66cda3
darksectordds <pechenskih.stas@ya.ru>
2022-12-15 19:00:50 +0300

    fix imports and global registration SPA

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,20 @@ require('./bootstrap');
 // Require Vue
 window.Vue = require('vue').default;
 
+// Axios
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+Vue.use(VueAxios, axios);
+
+// FontAwesome
+import { library } from '@fortawesome/fontawesome-svg-core'; // import the fontawesome core
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'; //import font awesome icon component
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons'; //add some free styles
+library.add(faCartShopping); //add icons to the library
+Vue.component('font-awesome-icon', FontAwesomeIcon); // add font awesome icon component
+
+Vue.config.productionTip = false;
+
 // Init own global variable
 Vue.prototype.$models = {};
 
@@ -13,7 +27,7 @@ Vue.use(ProductModel);
 Vue.use(UtilsPlugin);
 
 // Register Vue Components
-Vue.component('template-pizza-card-component', require('./components/templates/pizza/TemplatePizzaCardComponent.vue').default);
+Vue.component('app-component', require('./components/modules/AppComponent').default);
 
 // router in single page web-application
 import routes from './routes';
