78c4939cf07b0002131334e24c75bb61f38f6fd6
darksectordds <pechenskih.stas@ya.ru>
2022-12-16 19:17:37 +0300

    fix non-auth identification user by session_id where session()->getId() was generait a new session_id

--- app/Http/Controllers/Api/CartController.php
+++ app/Http/Controllers/Api/CartController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\ListController;
 use App\Models\Cart;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Crypt;
 
 class CartController extends ListController
 {
@@ -12,9 +13,40 @@ class CartController extends ListController
 
     protected $orderByColumn = 'id';
 
+    /**
+     * Получение ID-сессии из cookie-запроса,
+     * который сохраняется в зависимости от конфига
+     * в .env (1 год сейчас).
+     *
+     * @param Request $request
+     * @return string
+     */
+    private function getSessionIdFromRequestCookie(Request $request)
+    {
+        // WARNING: не использовать session()->getId(),
+        // потому что эта функция генерирует новую сессию
+        // каждый раз!!!
+
+        $key = strtolower(config('app.name')).'_session';
+        $cookie_arr = $request->cookie();
+
+        $crypt_session_id = $cookie_arr[$key];
+
+        $hash = Crypt::decryptString($crypt_session_id);
+
+        return substr($hash, strpos($hash, '|') + 1);
+    }
+
+    /**
+     * Создание новой корзины или возврат сущ.
+     * не оплаченной!
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function create(Request $request)
     {
-        $session_id = session()->getId();
+        $session_id = $this->getSessionIdFromRequestCookie($request);
 
         $cart = $this->model::firstOrCreate([
             'session_id' => $session_id,
