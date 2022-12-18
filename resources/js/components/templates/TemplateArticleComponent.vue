6459426fcd4798111e598942ae14500e3a86096a
darksectordds <pechenskih.stas@ya.ru>
2022-12-18 14:36:29 +0300

    add common article template

---
+++ resources/js/components/templates/TemplateArticleComponent.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="pizza__article">
+        <div class="pizza__article__body">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'TemplateArticleComponent'
+    }
+</script>
